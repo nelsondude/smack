1ac25d8c90131f39aae3e3a668d20b4ff0e21c93
nelsondude <alexn1336@gmail.com>
2017-11-14 02:15:57 -0500

    Web Request Auth

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,8 +8,20 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+// URL Constants
+
+let BASE_URL = "https://ios11-chat-app.herokuapp.com/v1/"
+let URL_REGISTER = BASE_URL + "account/register"
+
 // Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+// Auth/Defaults
+let LOGGED_IN_KEY = "loggedIn"
+let TOKEN_KEY = "token"
+let USER_EMAIL = "userEmail"
